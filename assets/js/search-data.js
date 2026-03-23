ef816f6ca41d804b69dc100df1452ac1f5170cc7
hyeongyu-kim <77005104+hyeongyu-kim@users.noreply.github.com>
2026-03-23 12:24:13 +0000

    Deploying to gh-pages from @ hyeongyu-kim/hyeongyu-kim.github.io@3385997508f8425b2f4e087aaffc23dd32c966ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-activity",
+          title: "Activity",
+          description: "Challenges &amp; Awards.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/activity/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
